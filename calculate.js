d578b06bb93a9bbcf086035a219787d650d21bee
Shivam Kumar <142294240+ShivamK15493@users.noreply.github.com>
2026-02-13 12:14:12 +0530

    Comments added in calculate.js

--- calculate.js
+++ calculate.js
@@ -1,41 +1,42 @@
-let num1 = document.getElementById("number1");
-let num2 = document.getElementById("number2");
+let num1 = document.getElementById("number1"); // Accessing input 1 given by the user
+let num2 = document.getElementById("number2"); // Accessing input 2 given by the user
 
-const add = document.getElementById("addition");
-const sub = document.getElementById("subtraction");
-const mul = document.getElementById("multiply");
-const div = document.getElementById("division");
+const add = document.getElementById("addition");    // Accessing addition button
+const sub = document.getElementById("subtraction"); // Accessing Subtraction button  
+const mul = document.getElementById("multiply");    // Accessing Multiplication button
+const div = document.getElementById("division");    // Accessing Division button
 
 let answer = document.getElementById("ans");
 
 add.addEventListener("click",() =>
 {
-    let result = parseFloat(num1.value) + parseFloat(num2.value);
-    document.getElementById("ans").innerText=result;
+    let result = parseFloat(num1.value) + parseFloat(num2.value); // Addition of two input
+    document.getElementById("ans").innerText=result; // Result of the Additon given by the user 
 })
 
 sub.addEventListener("click",() =>
 {
-    let result = parseFloat(num1.value) - parseFloat(num2.value);
-    document.getElementById("ans").innerText=result;
+    let result = parseFloat(num1.value) - parseFloat(num2.value); // Subtraction of two input
+    document.getElementById("ans").innerText=result; // Result of the Subtraction given by the user 
 })
 
 mul.addEventListener("click",() =>
 {
-    let result = parseFloat(num1.value) * parseFloat(num2.value);
-    document.getElementById("ans").innerText=result;
+    let result = parseFloat(num1.value) * parseFloat(num2.value); // Multiplication of two input
+    document.getElementById("ans").innerText=result;  // Result of the Multiplication given by the user
 })
 
 div.addEventListener("click",() =>
 {
-    let result = parseFloat(num1.value) / parseFloat(num2.value);
-    document.getElementById("ans").innerText=result;
+    let result = parseFloat(num1.value) / parseFloat(num2.value); // Division of two input
+    document.getElementById("ans").innerText=result; // Result of the Division given by the user
 })
 
 let resett = document.getElementById("rese");
 resett.addEventListener("click",() =>
 {
-    location.reload();
+    location.reload();  // Reload the page
 })
+
 
 
